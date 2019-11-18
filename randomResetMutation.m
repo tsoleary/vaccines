d729d0191a5ff9b6e60639825bc5acee4c552f10
bwilli16 <33357883+bwilli16@users.noreply.github.com>
2019-11-18 18:20:48 -0500

    Add files via upload

---
+++ randomResetMutation.m
@@ -0,0 +1,14 @@
+function mutantChildren = randomResetMutation(parents,options,GenomeLength,FitnessFcn,state,thisScore,population,probMutating)
+
+    mutantChildren = population(parents,:);
+    
+    for i=1:size(parents)
+        for j=1:size(population,2)
+       
+            %mutate
+            if rand()<probMutating
+               mutantChildren(i,j)=ceil(rand()*(1/probMutating));
+            end
+        end
+    end
+end
