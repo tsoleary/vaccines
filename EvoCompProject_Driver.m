2379b3f54d174b6172d0d9944ada529a5fde65bf
bwilli16 <33357883+bwilli16@users.noreply.github.com>
2019-11-18 17:09:24 -0500

    Update EvoCompProject_Driver.m

--- EvoCompProject_Driver.m
+++ EvoCompProject_Driver.m
@@ -4,25 +4,32 @@
 % Blake Williams, Thomas O'Leary and Alex Burnham
 
 % Starting parameters:
-P = 5;
-N = 10;
+
+adj_mat_network=A; %%
+
+P = 10;
+N = 100;
 mutProb = 1/N; % probabilty of mutation
-probZero = 0.7; % probability allele is 0
+probZero = 0.9; % probability allele is 0
 thresh = 3; % set our threashold for what our weighted genome sums to
+threshold = .5;
+transcendence = 2;
 
 % create weighted binary genome
 genomeFlat = randsample([0 1], P*N, true, [probZero 1-probZero]);
 genomeMat = vec2mat(genomeFlat, N); % creates weighted genome matrix with N cols
 
 % run the normalizeation function (this goes in Blake's fitness function)
-normGenome = NormalizeGenome(genomeMat, thresh); 
+%normGenome = NormalizeGenome(genomeMat, thresh); 
 
 
 
 
 % call fitness function and adj_list with anonoymous function 
 % NETWORK is still required (something from Blake?)
-vaccineFitness = @(pop)(FitnessFunction(pop, adj_list_network));
+%vaccineFitness = @(pop)(SpreadingFitnessFcn(pop, adj_mat_network, threshold, transcendence ));
+ 
+
 
 % set options for ga toolbox for bitstring
 vaccineOpts = gaoptimset(...
@@ -31,19 +38,16 @@ vaccineOpts = gaoptimset(...
     'CrossoverFcn', @crossoversinglepoint, ...
     'CrossoverFraction', 0.2, ...
     'SelectionFcn',{@selectiontournament,2}, ...
+    'Vectorized','on',...
     'Generations', 500, ...
-    'Vectorized', 'on', ...
     'FitnessLimit', 0, ...
-    'MutationFcn', {@weightedMutation mutProb});
+    'MutationFcn', {@mutationuniform, mutProb});
+%s{@weightedMutation...
+                   %  'mutatinRate',mutProb,...
+                   %  'weight', probZero}
+    
 
 % run GA (fitnessFunction, genomeLength, passOptions)
-[x, fval] = ga(vaccineFitness, N, vaccineOpts);
+%[x, fval] = ga(vaccineFitness, N, vaccineOpts);
 
-
-
-
-
-
-
-
-
+[x,fval] = ga(@SpreadingFitnessFcn, N, vaccineOpts);
