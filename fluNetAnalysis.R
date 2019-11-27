a3805e3330923a3e1f74b04480e48c3c80d13aa5
alexburn17 <pburnham@uvm.edu>
2019-11-27 09:56:43 -0500

    updates

--- fluNetAnalysis.R
+++ fluNetAnalysis.R
@@ -3,40 +3,76 @@
 # November 25, 2019
 
 
-
-# read in our data (using partial data right now (only three reps)) - Alex
-RealData <- read.csv("realNWdata.csv", 
-                              header = FALSE,
-                              stringsAsFactors = FALSE)
-
 library(dplyr)
 library(ggplot2)
 
-names(RealData) <- c('Fitness', 'Func.Calls', "Transcendence", 'Network.Size')
 
-RealData$Transcendence <- as.character(RealData$Transcendence)
-RealData$Network.Size <- as.character(RealData$Network.Size)
-
-ggplot(RealData, aes(y = Fitness, x = Network.Size, fill = Transcendence)) +
-  geom_boxplot(alpha=0.7) + theme_bw()
-
-
-
-
-# read in our data (using partial data right now (only three reps)) - Alex
-toyData <- read.csv("toyNWdata.csv", 
+# read in our data for 20 reps of 3 trans values for 9 different nws
+RealData <- read.csv("realNWdata1.csv",
                      header = FALSE,
                      stringsAsFactors = FALSE)
 
+# add names
+names(RealData) <- c('Fitness', 'Func.Calls', "Transcendence", 'Network.Size')
+
+# convert to character values
+RealData$Transcendence <- as.character(RealData$Transcendence)
+RealData$Network.Size <- as.factor(RealData$Network.Size)
+
+# plot figures for fittness
+fr <- ggplot(RealData, aes(y = Fitness, x = Network.Size, fill = Transcendence)) + 
+  geom_boxplot() +
+  theme_minimal(base_size = 18) +
+  labs(x="Network Size", y = "Fitness (prop. supercritical)", title = 'Fitness by Real Networks') +
+  theme(legend.position=c(.2, .8)) +
+  scale_fill_manual(values = c('grey', 'steelblue', 'black'), name = "trans.")
+
+
+
+
+# read in our data 20 reps for four toy nworks for 2 trans values
+toyData <- read.csv("toyNWdata.csv",
+                    header = FALSE,
+                    stringsAsFactors = FALSE)
+
+# add names
 names(toyData) <- c('Fitness', 'Func.Calls', 'Network' , "Transcendence")
 
+# make numeric values character strings
 toyData$Network <- as.factor(toyData$Network)
 toyData$Transcendence <- as.character(toyData$Transcendence)
 
-toyData$Network <- factor(toyData$Network, levels = c('1', '2', '3','4'), 
-       labels = c('lattice', 'star', 'chain', 'Erdos-Renyi'))
+# change factor names to real names of nws
+toyData$Network <- factor(toyData$Network, levels = c('1', '2', '3','4'),
+                          labels = c('lattice', 'star', 'chain', 'Erdos-Renyi'))
+# plot figures for fittness
+ft <- ggplot(toyData, aes(y = Fitness, x = Network, fill = Transcendence)) +
+  geom_boxplot() +
+  theme_minimal(base_size = 18) +
+  labs(x="Network Type", y = "Fitness (prop. supercritical)", title = 'Fitness by Toy Networks') +
+  theme(legend.position=c(.8, .8)) + 
+  scale_fill_manual(values = c('grey', 'steelblue'), name = "trans.")
 
-ggplot(toyData, aes(y = Fitness, x = Network, fill = Transcendence)) +
-  geom_boxplot(alpha=0.7) + theme_bw()
+
+# plot figures for calls
+ct <- ggplot(toyData, aes(y = Func.Calls, x = Network, fill = Transcendence)) +
+  geom_boxplot() +
+  theme_minimal(base_size = 18) +
+  labs(x="Network Type", y = "# Function Calls", title = 'Function Calls by Toy Networks') +
+  theme(legend.position=c(.8, .8)) + 
+  scale_fill_manual(values = c('grey', 'steelblue'), name = "trans.")
+
+
+# plot figures for calls
+cr <- ggplot(RealData, aes(y = Func.Calls, x = Network.Size, fill = Transcendence)) +
+  geom_boxplot() +
+  theme_minimal(base_size = 18) +
+  labs(x="Network Type", y = "# Function Calls", title = 'Function Calls by Real Networks') +
+  theme(legend.position=c(.8, .8)) + 
+  scale_fill_manual(values = c('grey', 'steelblue', 'black'), name = "trans.")
+
+library(cowplot)
+
+cowplot::plot_grid(ft, ct, fr, cr, labels = c("A", "B", "C", "D"), align = "v")
 
 
