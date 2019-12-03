66366d70af9bc9782fcd0f41972204a7bd89d76c
alexburn17 <pburnham@uvm.edu>
2019-12-03 08:57:05 -0500

    file dump

--- fluNetAnalysis.R
+++ fluNetAnalysis.R
@@ -5,10 +5,12 @@
 
 library(dplyr)
 library(ggplot2)
+library(scales)
+library(cowplot)
 
 
 # read in our data for 20 reps of 3 trans values for 9 different nws
-RealData <- read.csv("realNWdata1.csv",
+RealData <- read.csv("realNWdata2.csv",
                      header = FALSE,
                      stringsAsFactors = FALSE)
 
@@ -19,19 +21,24 @@ names(RealData) <- c('Fitness', 'Func.Calls', "Transcendence", 'Network.Size')
 RealData$Transcendence <- as.character(RealData$Transcendence)
 RealData$Network.Size <- as.factor(RealData$Network.Size)
 
+RealData$Log_fitness <- -log10(RealData$Fitness + 10^-5) # log10(RealData$Fitness+1)
+
+
+
 # plot figures for fittness
 fr <- ggplot(RealData, aes(y = Fitness, x = Network.Size, fill = Transcendence)) + 
   geom_boxplot() +
   theme_minimal(base_size = 18) +
-  labs(x="Network Size", y = "Fitness (prop. supercritical)", title = 'Fitness by Real Networks') +
-  theme(legend.position=c(.1, .5)) +
-  scale_fill_manual(values = c('grey', 'steelblue', 'black'), name = "trans.")
+  labs(x="Network Size", y = "prop. supercritical") +
+  theme(legend.position = 'top',axis.text.x = element_text(angle = 45, hjust = 1)) +
+  scale_fill_manual(values = c('grey', 'steelblue', 'black'), name = "Trans:") + 
+  coord_cartesian(ylim = c(0,.015)) #+ scale_y_log10()
 
 
 
 
 # read in our data 20 reps for four toy nworks for 2 trans values
-toyData <- read.csv("toyNWdata.csv",
+toyData <- read.csv("toyNWdataFinal.csv",
                     header = FALSE,
                     stringsAsFactors = FALSE)
 
@@ -44,35 +51,76 @@ toyData$Transcendence <- as.character(toyData$Transcendence)
 
 # change factor names to real names of nws
 toyData$Network <- factor(toyData$Network, levels = c('1', '2', '3','4'),
-                          labels = c('lattice', 'star', 'chain', 'Erdos-Renyi'))
+                          labels = c('lattice', 'star', 'chain', 'E-R'))
 # plot figures for fittness
-ft <- ggplot(toyData, aes(y = Fitness, x = Network, fill = Transcendence)) +
+ft <- ggplot(toyData, aes(y = log10(Fitness), x = Network, fill = Transcendence)) +
   geom_boxplot() +
   theme_minimal(base_size = 18) +
-  labs(x="Network Type", y = "Fitness (prop. supercritical)", title = 'Fitness by Toy Networks') +
-  theme(legend.position=c(.1, .5)) + 
-  scale_fill_manual(values = c('grey', 'steelblue'), name = "trans.")
+  labs(x="Network Type", y = "prop. supercritical") +
+  theme(legend.position = 'top',axis.text.x = element_text(angle = 45, hjust = 1)) + 
+  scale_fill_manual(values = c('grey', 'steelblue', 'black'), name = "Trans:") + coord_cartesian(ylim = c(0,.4))
 
 
 # plot figures for calls
-ct <- ggplot(toyData, aes(y = Func.Calls, x = Network, fill = Transcendence)) +
+ct <- ggplot(toyData, aes(y = log10(Func.Calls), x = Network, fill = Transcendence)) +
   geom_boxplot() +
   theme_minimal(base_size = 18) +
-  labs(x="Network Type", y = "# Function Calls", title = 'Calls by Toy Networks') +
-  theme(legend.position=c(2, 2)) + 
-  scale_fill_manual(values = c('grey', 'steelblue'), name = "trans.")
-
+  labs(x="Network Type", y = "log10(func. calls)") +
+  theme(legend.position = 'top',axis.text.x = element_text(angle = 45, hjust = 1)) + 
+  scale_fill_manual(values = c('grey', 'steelblue', 'black'), name = "Trans:") + coord_cartesian(ylim = c(2,4.5))
 
 # plot figures for calls
-cr <- ggplot(RealData, aes(y = Func.Calls, x = Network.Size, fill = Transcendence)) +
+cr <- ggplot(RealData, aes(y = log10(Func.Calls), x = Network.Size, fill = Transcendence)) +
   geom_boxplot() +
   theme_minimal(base_size = 18) +
-  labs(x="Network Type", y = "# Function Calls", title = 'Calls by Real Networks') +
-  theme(legend.position=c(2, 2)) + 
-  scale_fill_manual(values = c('grey', 'steelblue', 'black'), name = "trans.")
+  labs(x="Network Size", y = "log10(func. calls)") +
+  theme(legend.position = 'top',axis.text.x = element_text(angle = 45, hjust = 1)) + 
+  scale_fill_manual(values = c('grey', 'steelblue', 'black'), name = "Trans:") + coord_cartesian(ylim = c(2,4.5))
 
-library(cowplot)
+# plot the four plots together
+cowplot::plot_grid(ft,fr,ct,cr, labels = c("A", "B", "C", "D"), align = "v")
 
-cowplot::plot_grid(ft, ct, fr, cr, labels = c("A", "B", "C", "D"), align = "v")
 
 
+# vector between ranges of real net sizes
+N <- 20:1430
+
+# vector of function calls required to brut force networks with 1 to 4 vaccines
+kn <- c(choose(k=1, n=N), choose(k=2, n=N), choose(k=3, n=N), choose(k=4, n=N))
+
+# rep the number of N vals for plotting
+Nvec <- rep(N, 4)
+
+# rep K values
+Kvec <- as.character(c(rep(1, length(N)),rep(2, length(N)),rep(3, length(N)),rep(4, length(N))))
+
+# create a df
+chooseDF <- data.frame(kn, Nvec, as.character(Kvec))
+
+library(plyr)
+# summarise values for actual function calls
+DF3 <- ddply(RealData, c("Network.Size"), summarise, 
+             n = length(Func.Calls),
+             mean = mean(Func.Calls, na.rm=TRUE),
+             sd = sd(Func.Calls, na.rm=TRUE),
+             se = sd / sqrt(n),
+             upper = mean+sd,
+             lower = mean-sd)
+
+
+# work on function call plot
+ggplot(chooseDF, aes(x=Nvec, y = kn, group = Kvec, color=Kvec)) + 
+  geom_line(size=1.5) + theme_classic(base_size = 18) +
+  labs(y='log10(function calls)', x='network size', color='# Vaccines:') +
+  theme(legend.position = 'top') +
+  annotate("point", x = as.numeric(as.character(DF3$Network.Size)), 
+           y = as.numeric(DF3$mean), 
+           colour = "blue") +
+  annotate('segment', x = as.numeric(as.character(DF3$Network.Size)), 
+           y = DF3$lower, xend = as.numeric(as.character(DF3$Network.Size)), 
+                   yend = DF3$upper) + scale_y_log10()
+
+
+x=split(RealData, RealData$Transcendence)
+temp <- x$`1`[x$`1`$Network.Size=='1430', ]
+temp$Fitness
